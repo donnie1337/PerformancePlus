ee3a40000c5c613a1a268ed12446599ba6b47a1b
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-21 08:39:16 -0300

    feat: torna limite o comando principal de diagnóstico

--- src/main/java/com/performanceplus/PerformancePlus.java
+++ src/main/java/com/performanceplus/PerformancePlus.java
@@ -73,7 +73,7 @@ public class PerformancePlus extends JavaPlugin {
 
     private void registerCommands() {
         PerformancePlusCommand commandExecutor = new PerformancePlusCommand(this);
-        PluginCommand command = getCommand("performanceplus");
+        PluginCommand command = getCommand("limite");
         if (command != null) {
             command.setExecutor(commandExecutor);
             command.setTabCompleter(commandExecutor);
